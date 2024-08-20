074a53effc61ef6519ac2bd85cafded9820c2550
Ankit Verma <ankitv78389@gmail.com>
2024-08-20 23:52:24 +0530

    :books:[docs][changed readme file.]

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -10,21 +10,23 @@
 
 // export default Quiz;
 
-
-import { useEffect } from "react";
+import { useEffect.useSt } from "react";
 
 const Quiz = () => {
+
+  const[quizData,setQuizData]=([])
   useEffect(() => {
     fetch(
       "https://opentdb.com/api.php?amount=5&type=multiple&token=28f0e1e962a4f649b0fad76bb8b4f257720605cceea956ce67cbbe31aed9b1ee"
     )
       .then((resp) => resp.json())
-      .then((data) => {
-        const dataArr = data.map((dataElement)=>
+      .then((data) => { 
         return(
-          <div></h1>{data.question}<h1></div>
-        ))
-      });
+          
+        console.log(data)
+        
+      ) 
+      })
   }, []);
   return (
     <div>
