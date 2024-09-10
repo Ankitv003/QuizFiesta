ded575c44afa937f46b4516a9c7ab3e0145dd743
Ankit Verma <ankitv78389@gmail.com>
2024-09-10 23:07:08 +0530

    :hammer:[refractor][removed some code , need to add logic for third page]

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -41,69 +41,6 @@
 
 // export default Quiz;
 
-// import { nanoid } from "nanoid";
-// import { useState } from "react";
-
-// const Quiz = (props) => {
-//   const [selectedAnswers, setSelectedAnswers] = useState({});
-
-//   const handleAnswerClick = (questionId, answer) => {
-//     setSelectedAnswers((prevSelectedAnswers) => ({
-//       ...prevSelectedAnswers,
-//       [questionId]: answer,
-//     }));
-//   };
-
-//   const isCorrect = (questionId, answer) => {
-//     const quiz = props.quizData.find((q) => q.question === questionId);
-//     return quiz && quiz.correct_answer === answer;
-//   };
-
-//   return (
-//     <div className="quiz-container">
-//       {props.quizData.map((quiz) => {
-//         const selectedAnswer = selectedAnswers[quiz.question];
-
-//         return (
-//           <div className="quiz-info" key={nanoid()}>
-//             <h2 className="quiz-question">{quiz.question}</h2>
-//             <div className="quiz-buttons">
-//               {[quiz.correct_answer, ...quiz.incorrect_answers].map(
-//                 (answer) => {
-//                   const isSelected = selectedAnswer === answer;
-//                   const isAnswerCorrect = isCorrect(
-//                     quiz.question,
-//                     selectedAnswer
-//                   );
-
-//                   return (
-//                     <button
-//                       key={nanoid()}
-//                       onClick={() => handleAnswerClick(quiz.question, answer)}
-//                       className={`answers ${
-//                         isSelected
-//                           ? isAnswerCorrect
-//                             ? "correct"
-//                             : "wrong"
-//                           : ""
-//                       }`}
-//                       disabled={!!selectedAnswer}
-//                     >
-//                       {answer}
-//                     </button>
-//                   );
-//                 }
-//               )}
-//             </div>
-//           </div>
-//         );
-//       })}
-//       <button className="show-answers">Show Answers</button>
-//     </div>
-//   );
-// };
-
-// export default Quiz;
 import { nanoid } from "nanoid";
 import { useState, useEffect } from "react";
 
